0fbe104847af1752fa183f79b03f3948e7060c23
befk1970 <109320353+befk1970@users.noreply.github.com>
2022-07-20 00:20:11 -0300

    Add files via upload

--- palindromo.py
+++ palindromo.py
@@ -1,11 +1,11 @@
 def es_palindromo(palabra):
     i=0
     sigo=True
-    while sigo and i < ((len(palabra)-1)/2):
-        letra1 = palabra[i]
-        letra2 = palabra[len(palabra)-1-i]
+    palabra_m = palabra.lower()
+    while sigo and i < ((len(palabra_m)-1)/2):
+        letra1 = palabra_m[i]
+        letra2 = palabra[len(palabra_m)-1-i]
         if letra1 != letra2:
             sigo = False
-        
         i+=1    
     return sigo
